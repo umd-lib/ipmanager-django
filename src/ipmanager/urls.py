4c2aec7c99d06b373a606537162948ef487f3010
Mackenzie Kilgore <mkilgore@umd.edu>
2025-01-10 16:50:52 -0500

    LIBITD-2532. Created view for the groups/group_key route
    Created a class for the group_key view and added URL configuration for it
    
    https://umd-dit.atlassian.net/browse/LIBITD-2532

--- src/ipmanager/urls.py
+++ src/ipmanager/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-
 from ipmanager.api.views import GroupsView
+from ipmanager.api.views import Group_KeyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('groups/', GroupsView.as_view())
+    path('groups/<group_key>/', Group_KeyView.as_view())
 ]
