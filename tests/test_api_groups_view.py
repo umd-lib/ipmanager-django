f128a114b2791154e4babb238fbd93d0cc7e9677
Neola Lauren Dsouza <nldsouza@umd.edu>
2025-01-14 13:49:49 -0500

    LIBITD-2537. Updated groups API tests to remove more explicit keys() calls
    https://umd-dit.atlassian.net/browse/LIBITD-2537

--- tests/test_api_groups_view.py
+++ tests/test_api_groups_view.py
@@ -5,14 +5,14 @@ def test_valid_response(client):
     response_data = response.json()
     assert response.status_code == 200
     assert "@id" in response_data
-    assert "groups" in response_data and isinstance(response.json().get("groups"), list)
+    assert "groups" in response_data and isinstance(response_data.get("groups"), list)
     
     # Testing if the groups key's value is an empty list or a valid list
-    is_empty = "groups" in response_data and len(response.json()["groups"]) == 0
+    is_empty = "groups" in response_data and len(response_data["groups"]) == 0
     
     if not is_empty:
-        group_keys = response.json()["groups"][0].keys()
-        has_entries = "@id" in group_keys and "key" in group_keys and "name" in group_keys
+        group_data = response_data["groups"][0]
+        has_entries = "@id" in group_data and "key" in group_data and "name" in group_data
     
     assert is_empty or has_entries
 
