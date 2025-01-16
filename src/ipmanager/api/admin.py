052a271ae37b682103954c4f157f0006cab627a1
Mackenzie Kilgore <mkilgore@umd.edu>
2025-01-16 16:14:48 -0500

    LIBITD-2540. Created admin file and edited settings
    Registered model classes with admin site. Modified time in settings to be new york time.
    
    https://umd-dit.atlassian.net/browse/LIBITD-2540

--- src/ipmanager/api/admin.py
+++ src/ipmanager/api/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Group, IPRange, Relation
+
+class GroupAdmin(admin.ModelAdmin):
+  pass
+
+admin.site.register(Group, GroupAdmin)
+
+class IPRangeAdmin(admin.ModelAdmin):
+  pass
+
+admin.site.register(IPRange, IPRangeAdmin)
+
+class RelationAdmin(admin.ModelAdmin):
+  pass
+
+admin.site.register(Relation, RelationAdmin)
